6a6ba8a2d15f5d3fbc9984ad84d77d77bb3404ad
skylerhogan <skyler.hogan@gmail.com>
2021-07-01 12:21:53 -0500

    added authentication and login/logout

--- src/main/java/org/launchcode/codingevents/data/TagRepository.java
+++ src/main/java/org/launchcode/codingevents/data/TagRepository.java
@@ -4,9 +4,6 @@ import org.launchcode.codingevents.models.Tag;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Created by Chris Bay
- */
 @Repository
 public interface TagRepository extends CrudRepository<Tag, Integer> {
 }
